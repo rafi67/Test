f442b15708aa6fdaf965d0380874e9271a435f2b
Rafi Siddique <rafisiddique652@gmail.com>
2021-06-10 16:24:05 +0600

    Signal Handling Test 3

---
+++ SignalHandling/Test3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <csignal>
+
+using namespace std;
+
+volatile sig_atomic_t signalled;
+
+void handler(int sig) {
+    signalled = sig;
+}
+
+int main() {
+
+    signal(SIGABRT,handler);
+    cout << "Before signal handled" << endl;
+    cout << "Signal = " << signalled << endl;
+    raise(SIGABRT);
+    cout << "After signal handled" << endl;
+    cout << "Signal = " << signalled << endl;
+
+    return 0;
+
+}
