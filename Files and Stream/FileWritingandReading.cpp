2c231095841af978ee4ac77fda1457cc55440a0f
Rafi Siddique <rafisiddique652@gmail.com>
2021-06-11 06:34:39 +0600

    code modified

--- Files and Stream/FileWritingandReading.cpp
+++ Files and Stream/FileWritingandReading.cpp
@@ -3,33 +3,25 @@
 
 using namespace std;
 
-class Read {
+class WriteRead {
 
     public:
-        void read(string filename) {
-            string file;
-            fstream filestream(filename);
-            while(getline(filestream,file)) {
-                cout << file << endl;
-            }
-            filestream.close();
-        }
-
-};
-
-class Write {
-
-    public:
-        void write() {
-            char input[25];
+        void writeread() {
             fstream filestream("text.txt");
+            string text;
             cout << "Enter your name: ";
-            cin.getline(input, 25);
-            filestream << input << endl;
+            getline(cin, text);
+            filestream << text << endl;
             cout << "Enter your age: ";
-            cin >> input;
-            cin.ignore();
-            filestream << input << endl;
+            cin >> text;
+            filestream << text << endl;
+            filestream.close();
+
+            filestream.open("text.txt");
+            while(getline(filestream,text)) {
+                cout << text << endl;
+            }
+
             filestream.close();
         }
 
@@ -38,9 +30,9 @@ class Write {
 
 int main() {
 
-    Write w;
-    Read r;
-    w.write();
-    r.read("text.txt");
+    WriteRead wr;
+    wr.writeread();
+
+    return 0;
 
 }
