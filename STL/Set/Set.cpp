f7aaabdb70e2c086f94e1633dc58b8cef2d68d41
Rafi Siddique <rafisiddique652@gmail.com>
2021-06-15 15:45:02 +0600

    STL Set equal_range function

--- STL/Set/Set.cpp
+++ STL/Set/Set.cpp
@@ -38,9 +38,25 @@ class S2 {
 
 };
 
+class S3 {
+
+    public:
+        void show() {
+            set<short>s;
+            s.insert(4);
+            s.insert(1);
+            s.insert(6);
+            pair<set<short>::const_iterator, set<short>::const_iterator>ret;
+            ret = s.equal_range(10);
+            cout << "lower bound of 10 is " << *ret.first << endl;
+            cout << "upper bound of 10 is " << *ret.second << endl;
+        }
+
+};
+
 int main() {
 
-    S2 s;
+    S3 s;
     s.show();
 
     return 0;
