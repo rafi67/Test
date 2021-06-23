aa7ef80f5ed0dd55a2cf4ac2ea005878df170775
Rafi Siddique <rafisiddique652@gmail.com>
2021-06-23 13:32:19 +0600

    Regex 5

--- Regex/Regex5.cpp
+++ Regex/Regex5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "PrintMatches.h"
+#include "Result.h"
 
 using namespace std;
 
@@ -9,8 +10,10 @@ int main() {
     cout << "Enter a valid email address: ";
     cin >> email;
     //string s = "db@aol.com m@.com @apple.com db@.com";
-    regex r ("[\\w._+%+-]{0,100}[@]{1}[\\w]{0,20}.[\\w]{2,3}");
-    PrintMatches(email, r);
+    regex r ("[\\w._%+-]{0,100}[@]{1}[\\w]{0,100}[.]{1}[\\w]{0,10}.[\\w]{0,10}");
+    if(isValid(email, r)) PrintMatches(email, r);
+
+    else cout << "Your email is not valid try again...." << endl;
     
     return 0;
 
